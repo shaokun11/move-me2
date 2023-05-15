f27bb5188d536cf26811a587300d87a4430184a8
shaokun <605416017@qq.com>
2023-05-15 10:32:38 +0800

    fixed

--- subnet/src/vm/mod.rs
+++ subnet/src/vm/mod.rs
@@ -686,7 +686,10 @@ impl ChainVm for Vm
             let prnt_blk = state_b.get_block(&vm_state.preferred).await.unwrap();
             let unix_now = Utc::now().timestamp() as u64;
             let core_pool = self.core_mempool.as_ref().unwrap().read().await;
-            let tx_arr = core_pool.get_batch(1000, 1024000, true, HashSet::new(),true);
+            let tx_arr = core_pool.get_batch(1000,
+                                             1024000,
+                                             true,
+                                             true,vec![]);
             println!("----build_block pool tx count-------{}------", tx_arr.clone().len());
             drop(core_pool);
             let executor = self.executor.as_ref().unwrap().read().await;
