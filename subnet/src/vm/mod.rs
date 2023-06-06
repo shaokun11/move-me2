3927393c5531020f6575bfbd5e4c6137f0386384
shaokun <605416017@qq.com>
2023-06-06 13:50:50 +0800

    设置tps的目录

--- subnet/src/vm/mod.rs
+++ subnet/src/vm/mod.rs
@@ -62,7 +62,7 @@ use crate::api::chain_handlers::{AccountStateArgs, ChainHandler, ChainService, R
 use crate::api::static_handlers::{StaticHandler, StaticService};
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
-const MOVE_DB_DIR: &str = "aptos-chain-data";
+const MOVE_DB_DIR: &str = "aptos-chain-data-tps";
 
 #[derive(Serialize, Deserialize, Clone)]
 pub struct AptosData(
