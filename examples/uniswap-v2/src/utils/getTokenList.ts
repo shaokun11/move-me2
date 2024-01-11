510e0b01d63b9f852afb71bb9909e11b39653f77
kuolijiwa <116632001+kuolijiwa@users.noreply.github.com>
2024-01-11 11:18:23 +0800

    Update getTokenList.ts
    Remove unnecessary requests.

--- examples/uniswap-v2/src/utils/getTokenList.ts
+++ examples/uniswap-v2/src/utils/getTokenList.ts
@@ -17,55 +17,78 @@ export default async function getTokenList(
   listUrl: string,
   resolveENSContentHash: (ensName: string) => Promise<string>
 ): Promise<TokenList> {
-  const parsedENS = parseENSAddress(listUrl)
-  let urls: string[]
-  if (parsedENS) {
-    let contentHashUri
-    try {
-      contentHashUri = await resolveENSContentHash(parsedENS.ensName)
-    } catch (error) {
-      console.debug(`Failed to resolve ENS name: ${parsedENS.ensName}`, error)
-      throw new Error(`Failed to resolve ENS name: ${parsedENS.ensName}`)
-    }
-    let translatedUri
-    try {
-      translatedUri = contenthashToUri(contentHashUri)
-    } catch (error) {
-      console.debug('Failed to translate contenthash to URI', contentHashUri)
-      throw new Error(`Failed to translate contenthash to URI: ${contentHashUri}`)
-    }
-    urls = uriToHttp(`${translatedUri}${parsedENS.ensPath ?? ''}`)
-  } else {
-    urls = uriToHttp(listUrl)
-  }
-  for (let i = 0; i < urls.length; i++) {
-    const url = urls[i]
-    const isLast = i === urls.length - 1
-    let response
-    try {
-      response = await fetch(url)
-    } catch (error) {
-      console.debug('Failed to fetch list', listUrl, error)
-      if (isLast) throw new Error(`Failed to download list ${listUrl}`)
-      continue
-    }
 
-    if (!response.ok) {
-      if (isLast) throw new Error(`Failed to download list ${listUrl}`)
-      continue
+  // console.log('gettoken-111')
+    const json:any = {
+      "name": "1inch",
+      "timestamp": "2022-04-06T22:19:09+00:00",
+      "version": {
+          "major": 145,
+          "minor": 0,
+          "patch": 0
+      },
+      "keywords": [
+          "1inch",
+          "default",
+          "list"
+      ],
+      tokens:[]
     }
+    json.tokens.push(
+      ...G_CONFIG.tokens
+    )
 
-    const json = await response.json()
-    if (!tokenListValidator(json)) {
-      const validationErrors: string =
-        tokenListValidator.errors?.reduce<string>((memo, error) => {
-          const add = `${error.dataPath} ${error.message ?? ''}`
-          return memo.length > 0 ? `${memo}; ${add}` : `${add}`
-        }, '') ?? 'unknown error'
-      throw new Error(`Token list failed validation: ${validationErrors}`)
-    }
-    json.tokens=G_CONFIG.tokens
     return json
-  }
-  throw new Error('Unrecognized list URL protocol.')
+  // const parsedENS = parseENSAddress(listUrl)
+  // let urls: string[]
+  
+  // if (parsedENS) {
+  //   let contentHashUri
+  //   try {
+  //     contentHashUri = await resolveENSContentHash(parsedENS.ensName)
+  //   } catch (error) {
+  //     console.debug(`Failed to resolve ENS name: ${parsedENS.ensName}`, error)
+  //     throw new Error(`Failed to resolve ENS name: ${parsedENS.ensName}`)
+  //   }
+  //   let translatedUri
+  //   try {
+  //     translatedUri = contenthashToUri(contentHashUri)
+  //   } catch (error) {
+  //     console.debug('Failed to translate contenthash to URI', contentHashUri)
+  //     throw new Error(`Failed to translate contenthash to URI: ${contentHashUri}`)
+  //   }
+  //   urls = uriToHttp(`${translatedUri}${parsedENS.ensPath ?? ''}`)
+  // } else {
+  //   urls = uriToHttp(listUrl)
+  // }
+  // for (let i = 0; i < urls.length; i++) {
+  //   const url = urls[i]
+  //   const isLast = i === urls.length - 1
+  //   let response
+  //   try {
+  //     response = await fetch(url)
+  //   } catch (error) {
+  //     console.debug('Failed to fetch list', listUrl, error)
+  //     if (isLast) throw new Error(`Failed to download list ${listUrl}`)
+  //     continue
+  //   }
+
+  //   if (!response.ok) {
+  //     if (isLast) throw new Error(`Failed to download list ${listUrl}`)
+  //     continue
+  //   }
+    
+    // const json = await response.json()
+    // if (!tokenListValidator(json)) {
+    //   const validationErrors: string =
+    //     tokenListValidator.errors?.reduce<string>((memo, error) => {
+    //       const add = `${error.dataPath} ${error.message ?? ''}`
+    //       return memo.length > 0 ? `${memo}; ${add}` : `${add}`
+    //     }, '') ?? 'unknown error'
+    //   throw new Error(`Token list failed validation: ${validationErrors}`)
+    // }
+    // json.tokens=G_CONFIG.tokens
+    
+  // }
+  // throw new Error(/'Unrecognized list URL protocol.')
 }
