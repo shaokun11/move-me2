bfb5be6ef5dc47c4da041090aabac13980b196f0
shaokun <605416017@qq.com>
2024-02-22 17:29:20 +0800

    根据node id来创建db的路径

--- subnet/src/vm/mod.rs
+++ subnet/src/vm/mod.rs
@@ -1009,18 +1009,19 @@ impl Vm {
         );
         self.signer = Some(signer.clone());
         let genesis_txn = Transaction::GenesisTransaction(WriteSetPayload::Direct(genesis));
-        // let p = format!(
-        //     "{}/{}",
-        //     dirs::home_dir().unwrap().to_str().unwrap(),
-        //     MOVE_DB_DIR
-        // );
-        // if !fs::metadata(p.clone().as_str()).is_ok() {
-        //     fs::create_dir_all(p.as_str()).unwrap();
-        // }
-        // let db = DbReaderWriter::wrap(AptosDB::new_for_test(p.as_str()));
-        let tmp_dir = TempPath::new();
-        tmp_dir.create_as_dir().unwrap();
-        let db = DbReaderWriter::wrap(AptosDB::new_for_test(&tmp_dir));
+        let node_id = self.state.read().await.ctx.unwrap().node_id;
+        let p = format!(
+            "{}/{}",
+            dirs::home_dir().unwrap().to_str().unwrap(),
+            hex::encode(node_id.to_vec())
+        );
+        if !fs::metadata(p.clone().as_str()).is_ok() {
+            fs::create_dir_all(p.as_str()).unwrap();
+        }
+        let db = DbReaderWriter::wrap(AptosDB::new_for_test(p.as_str()));
+        // let tmp_dir = TempPath::new();
+        // tmp_dir.create_as_dir().unwrap();
+        // let db = DbReaderWriter::wrap(AptosDB::new_for_test(&tmp_dir));
         let waypoint = generate_waypoint::<AptosVM>(&db.1, &genesis_txn);
         match waypoint {
             Ok(w) => {
