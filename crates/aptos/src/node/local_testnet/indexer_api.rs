0e20a686d28562324e9ee4827938450ab5ccc8dc
lei-1993 <lei@movemnt.dev>
2024-04-29 20:41:30 +0800

    remote access

--- crates/aptos/src/node/local_testnet/indexer_api.rs
+++ crates/aptos/src/node/local_testnet/indexer_api.rs
@@ -167,7 +167,7 @@ impl ServiceManager for IndexerApiManager {
             extra_hosts: Some(vec!["host.docker.internal:host-gateway".to_string()]),
             port_bindings: Some(hashmap! {
                 self.indexer_api_port.to_string() => Some(vec![PortBinding {
-                    host_ip: Some("127.0.0.1".to_string()),
+                    host_ip: Some("0.0.0.0".to_string()),
                     host_port: Some(self.indexer_api_port.to_string()),
                 }]),
             }),
