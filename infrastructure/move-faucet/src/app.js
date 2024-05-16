c5941725d0de4917d20002b58f7640dd9e9cf5b5
tonyxia2023 <tony@movemnt.dev>
2024-05-16 05:07:06 +0000

    implement move  faucet , to test

--- infrastructure/move-faucet/src/app.js
+++ infrastructure/move-faucet/src/app.js
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
-import { SERVER_PORT } from './const.js';
+import { NODE_URL, SERVER_PORT } from './const.js';
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: '10mb' }));
@@ -13,17 +13,36 @@ const get_ip = (req) => {
         req.ip;
 }
 
-// app.post('/fund', async function (req, res) {
-//     const address = req.body.address;
-//     const result = await request('faucetWithCli', { ...option, is_bcs_format: true });
-//     res.send(result.data);
-// });
+const make_req_option = (req) => {
+    const opt = {
+        method: req.method,
+        url: NODE_URL + req.path,
+        params: req.query,
+        headers: req.headers
+    };
+    if (req.method.toLowerCase() === 'post' || req.method.toLowerCase() === 'put') {
+        opt.data = req.body;
+    }
+    delete opt.headers.host;
+    return opt
+}
 
-// app.post('/mint', async function (req, res) {
-//     const address = req.query.auth_key;
-//     const result = await request('faucetWithCli', { ...option, is_bcs_format: true });
-//     res.send(result.data);
-// });
+// for petra wallet faucet
+app.post('/fund', async function (req, res) {
+    opt.data.amount = parseInt(FAUCET_AMOUNT) * 1e8;
+    await axios(make_req_option(req));
+    res.send(response.data);
+    res.status(response.status);
+    res.setHeader('Content-Type', req.headers['Accept'])
+});
+// for aptos cli faucet
+app.post('/mint', async function (req, res) {
+    opt.data.amount = parseInt(FAUCET_AMOUNT) * 1e8;
+    await axios(make_req_option(req));
+    res.send(response.data);
+    res.status(response.status);
+    res.setHeader('Content-Type', req.headers['Accept'])
+});
 
 app.set('trust proxy', true);
 app.listen(SERVER_PORT, () => {
