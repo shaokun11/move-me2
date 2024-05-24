eeda4e65d840f96adc15c037bee358abb3cdc77e
tonyxia2023 <tony@movemnt.dev>
2024-05-24 10:42:22 +0000

    fixed

--- infrastructure/move-faucet-server/src/const.js
+++ infrastructure/move-faucet-server/src/const.js
@@ -5,6 +5,7 @@ export const FAUCET_LIMIT_DURATION = process.env.FAUCET_LIMIT_DURATION || 1;
 export const NODE_URL = process.env.NODE_URL
 export const FAUCET_NODE_URL = process.env.FAUCET_NODE_URL
 export const FAUCET_SENDER = process.env.FAUCET_SENDER
+export const RECAPTCHA_SECRET = process.env.RECAPTCHA_SECRET
 
 export const FAUCET_SENDER_ACCOUNT = AptosAccount.fromAptosAccountObject({
     privateKeyHex: FAUCET_SENDER,
--- infrastructure/move-faucet-server/src/provider.js
+++ infrastructure/move-faucet-server/src/provider.js
@@ -1,15 +1,19 @@
+import {RECAPTCHA_SECRET} from "./const.js";
 export async function googleRecaptcha(token) {
     // no secret key provided, just skip and return true
     if (!RECAPTCHA_SECRET) return true;
     if (!token) return false;
     const keys = RECAPTCHA_SECRET.split(',');
-    const result = await Promise.all(keys.map(key => fetch('https://www.google.com/recaptcha/api/siteverify', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-        body: `secret=${key}&response=${token}`,
-    })
-        .then(response => response.json())
-        .then(res => res.success)
-        .catch(() => false)));
-    return result.some(r => r);
+    for (const key of keys) {
+        const pass = await fetch('https://www.google.com/recaptcha/api/siteverify', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+            body: `secret=${key}&response=${token}`,
+        })
+            .then(response => response.json())
+            .then(res => res.success)
+            .catch(() => false);
+        if (pass) return true;
+    }
+    return false
 }
