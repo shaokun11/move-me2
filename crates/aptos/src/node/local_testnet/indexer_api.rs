1690789e474ffa210c8e88ab83e10759cb1b5ade
lei-1993 <lei@movemnt.dev>
2024-07-18 10:17:32 +0800

    debuger indexer

--- crates/aptos/src/node/local_testnet/indexer_api.rs
+++ crates/aptos/src/node/local_testnet/indexer_api.rs
@@ -208,7 +208,7 @@ impl ServiceManager for IndexerApiManager {
             ..Default::default()
         });
 
-        info!("Starting indexer API with this config: {:?}", config);
+        println!("Starting indexer API with this config: {:?}", config);
 
         let id = docker.create_container(options, config).await?.id;
 
