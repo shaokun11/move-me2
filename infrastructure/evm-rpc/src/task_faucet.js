fe19e0c72ebc9cfb8eede11cb63680991579af10
tonyxia2023 <tony@movemnt.dev>
2024-07-19 06:45:01 +0000

    fixed batch facuet eth

--- infrastructure/evm-rpc/src/task_faucet.js
+++ infrastructure/evm-rpc/src/task_faucet.js
@@ -29,7 +29,7 @@ async function run(faucet_amount, batch = 100) {
     const send_accounts = FAUCET_QUEUE.slice(0, batch);
     if (send_accounts.length > 0) {
         const payload = {
-            function: `0x1::batch_transfer::batch_transfer_evm`,
+            function: `0x1::evm::batch_transfer_evm`,
             type_arguments: [],
             arguments: [send_accounts.map(it => toBuffer(it.addr)), send_accounts.map(() => faucet_amount)],
         };
