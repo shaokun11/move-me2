604e42d641b724bcdd6fd79ed4e648fa5ddc4011
shaokun <605416017@qq.com>
2024-07-29 02:52:46 +0800

    set call contract tx type

--- infrastructure/evm-rpc/src/bridge.js
+++ infrastructure/evm-rpc/src/bridge.js
@@ -449,10 +449,10 @@ export async function estimateGas(info) {
     const nonce = await getNonce(info.from);
     if (!info.data) info.data = '0x';
     // Use maxFeePerGas to determine the type of transaction for MaxFeePerGas and gasPrice maybe both null
-    let type = Boolean(info.maxFeePerGas) ? '1' : '0';
+    let type = Boolean(info.maxFeePerGas) ? '2' : '1';
     let gasPrice = toBeHex(await getGasPrice());
     let maxFeePerGas = toBeHex(1);
-    if (type === '1' && info.maxFeePerGas) {
+    if (type === '2' && info.maxFeePerGas) {
         maxFeePerGas = toBeHex(info.maxFeePerGas);
     }
     const payload = {
@@ -469,7 +469,7 @@ export async function estimateGas(info) {
             maxFeePerGas, // max_fee_per_gas
             toBeHex(1), // max_priority_per_gas
             '0x',
-            type, //  if the tx type is 0 , only gas price is effect
+            type, //  if the tx type is 1 , only gas price is effect
         ],
     };
     const result = await client.view(payload);
@@ -730,7 +730,7 @@ async function callContractImpl(from, contract, calldata, value, version) {
             toBeHex(1),
             toBeHex(1),
             '0x',
-            '0',
+            '1',
         ],
     };
     const result = await client.view(payload, version);
