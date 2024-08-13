46fb2aab22b38a1d5a906b0d5b818bacd8470ef9
tonyxia2023 <tony@movemnt.dev>
2024-08-13 07:44:41 +0000

    add more 10 evm sender

--- infrastructure/evm-rpc/script/bal-check.js
+++ infrastructure/evm-rpc/script/bal-check.js
@@ -13,7 +13,19 @@ const evm_sender = {
         '0x1f565d5b45b9976a0ae73eb675b31625d1da9939a25c5ec829ffd564d6a0bebb',
         '0xb7293fa5b8ad8b89c70f9b329946286453235b63a8ebe1b62f6fa4e103a7fc12',
         '0x184df25baa560c9fac901d71147f8b0da3e75967e342cf0308749a416e00bdc6',
-        /**********        for sync node sender                          */
+        /** for base node sender  2 */
+        '0xd8b4bff3c6678cd3cea9c89fd66626f74eec5b3fab4d17f9312d243807a02e48',
+        '0x2acbc25fb5fde0dd8f8055d08887c828a8e5318ff6761884b62a8a1eb9535c0a',
+        '0x9350d69fddea50008f9d2ac82dc0c38e2ab257e5fed4f3954b5dfcaed0a84034',
+        '0x470b3a02a621b7cc456bf63879f39d7cc71c6ec9fe51dcf05c7c944d2dae48dc',
+        '0xdd23c83c517ddb9600c93381b5f617180ff92273fc210e79abfb783a920d88ba',
+        '0xae7622da2ff4f4dfd57321a52adfa1ae3b2792214f3072ff5b9dc1f998848481',
+        '0x36f86ebf12f6711973ed90af878d9667626fd0983d9547c1c626a701cb950392',
+        '0x2ac5ccf29419dfc7caf97544fa05a8c3954aef234077f97a194a800f03ec9815',
+        '0x1938ff1ea78f4a45b319accf86ce9dd8bdf8200c6220804497f97b26c172b4f8',
+        '0xd881ba68225d34293c174a48c3e5bd6a733410889bdde9ee3a5dc8d6ef915d10',
+        
+        /**********        for sync node-1 sender                          */
         '0xdaee19bf07c589088947f5aaeeb2ac83755c60e8abcee84696be50a6c90aa4b5',
         '0x4ff4cfeacf38acec73e9a31e2cf17f607ca867606468cd54f01eb5fe04c111fb',
     ],
