5669fe7dc5fc29306ca6a04acb83bb60e9c0ed8e
tonyxia2023 <tony@movemnt.dev>
2024-08-16 16:46:06 +0000

    sync

--- infrastructure/evm-rpc/src/client_wrapper.js
+++ infrastructure/evm-rpc/src/client_wrapper.js
@@ -1,13 +1,12 @@
 import BigNumber from 'bignumber.js';
 import { client } from './const.js';
-
+BigNumber.config({ EXPONENTIAL_AT: 100 });
 export class ClientWrapper {
     static async getBlockByHeight(height, withTxs) {
-     
         if (!withTxs) {
             return client.getBlockByHeight(height, false);
         }
-           // this api only return the first 100 transactions
+        // this api only return the first 100 transactions
         const block = await client.getBlockByHeight(height, true);
         let count = BigNumber(block.last_version).minus(block.first_version).toNumber() - 100 + 1;
         if (count > 0) {
@@ -16,7 +15,11 @@ export class ClientWrapper {
                     Array(count)
                         .fill()
                         .map((_, i) => {
-                            return client.getTransactionByVersion(block.first_version + i + 100);
+                            return client.getTransactionByVersion(
+                                BigNumber(block.first_version)
+                                    .plus(100 + i)
+                                    .toFixed(0),
+                            );
                         }),
                 )),
             );
