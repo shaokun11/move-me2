69f6dba9fa7985e05fbb399c629dfa52b57eff4b
lei-1993 <lei@movemnt.dev>
2024-08-26 22:37:02 +0800

    add log

--- third_party/move/move-binary-format/src/compatibility.rs
+++ third_party/move/move-binary-format/src/compatibility.rs
@@ -196,7 +196,7 @@ impl Compatibility {
             ).with_message(format!("Module Update Failure: Public function/struct signature of new module differs from existing module in {:?}::{}", old_module.address, old_module.name)));
         }
         if self.check_struct_layout && !struct_layout {
-            println!("Module Update Failure: Struct layout of new module differs from existing modul in {:?}::{}", old_module.address, old_module.name)
+            println!("Module Update Failure: Struct layout of new module differs from existing modul in {:?}::{}", old_module.address, old_module.name);
             return Err(PartialVMError::new(
                 StatusCode::BACKWARD_INCOMPATIBLE_MODULE_UPDATE,
             ).with_message(format!("Module Update Failure: Struct layout of new module differs from existing modul in {:?}::{}", old_module.address, old_module.name)));
